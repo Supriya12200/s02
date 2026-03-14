f11ded7dcfbbacf67d5a3f7652cd35d92aeca5b0
NAMA SRI LAKSHMI VENKATA SATYA SUPRIYA <namasupriyanama@gmail.com>
2026-03-14 19:19:22 +0530

    Delete bookingdelete directory

--- bookingdelete/src/main/java/com/klef/fsad/exam/repository/BookingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.klef.fsad.exam.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.klef.fsad.exam.model.Booking;
-
-public interface BookingRepository extends JpaRepository<Booking, Integer> {
-
-}
